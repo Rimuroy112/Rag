0d88fd4942814f251da2c16bc0a8f346e791c155
Rimu Roy <titashroy112@gmail.com>
2026-09-04 16:17:03 +0600

    FastAPI with Pydantic

--- FastAPI/FastAPI_Pydantic.py
+++ FastAPI/FastAPI_Pydantic.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -7,10 +8,14 @@ app = FastAPI()
 class post(BaseModel):
     title: str
     content: str
+    published: bool=True
+    rating: Optional[int] = None
 
 @app.post("/createposts")
 
 def create_posts(new_post: post):
-    print(new_post)
+    print(new_post.published)
+    print(new_post.rating)
+    print(new_post.dict())
     return{"data": "new_post"}
 
